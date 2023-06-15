bee934ba78d684d414181cc5c8054d8bac1ea856
Yul310 <hsy3102@gmail.com>
2023-06-14 23:50:04 -0400

    window size update and style.css started

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,15 +1,22 @@
 import * as THREE from 'three';
+import "./style.css"
 
 //Scene
 const scene = new THREE.Scene()
 
 //Create our sphere
-const geometry = new THREE.SphereGeometry(15,32,64)
+const geometry = new THREE.SphereGeometry(3,64,64)
 const material = new THREE.MeshStandardMaterial({
   color:"#00ff83",
 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+//Sizes
+const sizes ={
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
 
 //Light
 const light = new THREE.PointLight(0xffffff,1,100)
@@ -17,15 +24,15 @@ light.position.set(0, 10, 10)
 scene.add(light)
 
 //Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600)
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(45, sizes.width /sizes.height,0.1,100)
+camera.position.z = 20
 
 scene.add(camera)
 
 //Renderer
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({canvas})
-renderer.setSize(800, 600)
+renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
 
 
