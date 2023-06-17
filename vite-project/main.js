b9e2416c549e2fbd0f233e1bdb7d9c10e4d7494a
Yul310 <hsy3102@gmail.com>
2023-06-16 22:40:57 -0400

    orbit control added

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import "./style.css"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
 //Scene
 const scene = new THREE.Scene()
@@ -29,11 +30,23 @@ camera.position.z = 20
 
 scene.add(camera)
 
+
+
 //Renderer
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({canvas})
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(2)
 renderer.render(scene, camera)
+
+
+//Controls
+const controls = new OrbitControls(camera,canvas)
+controls.enableDamping = true
+controls.enablePan = false
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
 
 //Resize
 window.addEventListener("resize",() => {
@@ -50,6 +63,8 @@ window.addEventListener("resize",() => {
 
 
 const loop = () => {
+  controls.update
+  // mesh.position.x += 1
   renderer.render(scene, camera)
   window.requestAnimationFrame(loop)
 }
