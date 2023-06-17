0b6a39d4d610f90f1d6840c6e11186670d2080bf
Yul310 <hsy3102@gmail.com>
2023-06-17 02:38:44 -0400

    color change animation added

--- vite-project/main.js
+++ vite-project/main.js
@@ -10,6 +10,7 @@ const scene = new THREE.Scene()
 const geometry = new THREE.SphereGeometry(3,64,64)
 const material = new THREE.MeshStandardMaterial({
   color:"#00ff83",
+  roughness:0.4
 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
@@ -23,6 +24,7 @@ const sizes ={
 //Light
 const light = new THREE.PointLight(0xffffff,1,100)
 light.position.set(0, 10, 10)
+light.intensity = 1.25
 scene.add(light)
 
 //Camera
@@ -75,5 +77,31 @@ loop()
 //Tiemline manipulation
 const t1 = gsap.timeline({defaults: {duration:1}})
 t1.fromTo(mesh.scale,{z:0,x:0,y:0},{z:1,x:1,y:1})
+t1.fromTo('nav',{y:"-100%"},{y:"0%"})
+t1.fromTo(".title",{opacity:0},{opacity:1})
 
 
+//Mouse Animation Color
+let mouseDown = false
+let rgb = []
+window.addEventListener("mousedown", () => (mouseDown = true))
+window.addEventListener("mouseup", () => (mouseDown = false))
+
+window.addEventListener("mousemove",(e) => {
+if(mouseDown){
+  rgb = [
+    Math.round((e.pageX/sizes.width)*255),
+    Math.round((e.pageY/sizes.height)*255),
+    150
+  ]
+  console.log(rgb.join(','))
+  //Let's animate
+let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+console.log(newColor)
+gsap.to(mesh.material.color,{
+  r:newColor.r,
+  g:newColor.g,
+  b:newColor.b
+})
+}
+})
